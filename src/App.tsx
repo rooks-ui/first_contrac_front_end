60ef3725c428ef005c510056d1d2820003acaaf8
Faruk Umar <blackmallam05@gmail.com>
2024-09-21 12:44:48 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,80 @@
+import './App.css'
+import { TonConnectButton } from '@tonconnect/ui-react';
+import { useMainContract } from "./hooks/useMainContract";
+import { useTonConnect } from './hooks/useTonconnect';
+
+function App() {
+  const {
+    contract_address,
+    counter_value,
+    contract_balance,
+    sendIncrement,
+    sendDeposit,
+    sendWithdrawalRequest,
+  } = useMainContract();
+
+  const { connected } = useTonConnect;
+
+  return (
+    <div>
+      <div>
+        <TonConnectButton />
+      </div>
+      <div>
+        <div className='Card'>
+
+          <b>Our contract Address</b>
+          <div className='Hint'>{contract_address?.slice(0, 30) + "..."}</div>
+
+          <b>Our contract Balance</b>
+          {contract_balance && (
+            <div className='Hint'>{contract_balance}</div>
+          )}
+        </div>
+
+        <div className='Card'>
+          <b>Counter Value</b>
+          <div>{counter_value ?? "Loading..."}</div>
+        </div>
+
+        <div>
+
+          {connected && (
+            <a onClick ={() => {
+              sendIncrement();
+            }}
+            >
+              Increment by 5
+            </a>
+          )}
+
+          <br />
+
+          {connected && (
+            <a onClick ={() => {
+              sendDeposit();
+            }}
+            >
+              Request deposit of 1 TON
+            </a>
+          )}
+
+          <br />
+          
+          {connected && (
+            <a onClick ={() => {
+              sendWithdrawalRequest();
+            }}
+            >
+              Request 0.7TON withdrawal
+            </a>
+          )}
+          
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
